68e5e5f8760467dfbade194ce48b45e27dfffa87
chaerulcp <indonesianvidios@gmail.com>
2024-11-17 22:23:41 +0800

    update config.php

--- config.php
+++ config.php
@@ -4,9 +4,14 @@ $dbname = 'u609729740_sikapaiyya';
 $username = 'u609729740_root';
 $password = ':bG7w*n$o1Dw';
 
-try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Koneksi gagal: " . $e->getMessage();
+// Buat koneksi ke database
+$conn = mysqli_connect($host, $username, $password, $database);
+
+// Periksa koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+// Jika koneksi berhasil
+echo "Koneksi ke database berhasil";
+?>
