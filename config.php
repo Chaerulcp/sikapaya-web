5a65d4d8a628a56dcf661c04dad6d1216d691630
chaerulcp <indonesianvidios@gmail.com>
2024-11-18 10:33:47 +0800

    Refactor database connection string in config.php for improved readability

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
-$host = 'localhost';
-$dbname = 'u609729740_sikapaiyya';
+$dsn = 'mysql:host=localhost;dbname=u609729740_sikapaiyya';
 $username = 'u609729740_root';
 $password = ':bG7w*n$o1Dw';
 
 try {
-    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $db = new PDO($dsn, $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Koneksi gagal: " . $e->getMessage();
