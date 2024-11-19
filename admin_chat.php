8402d9a484df43aafcbee81d87c3bdf1e0a7df79
chaerulcp <indonesianvidios@gmail.com>
2024-11-19 20:09:55 +0800

    Replace Bootstrap bundle script with standard script for admin chat page

--- admin_chat.php
+++ admin_chat.php
@@ -149,7 +149,7 @@ if (isset($_POST['end_chat'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/bootstrap.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <title>Admin Chat</title>
     <style>
         .chat-container {
